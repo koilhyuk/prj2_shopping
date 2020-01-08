2aeed6478fa8f47e24a639ccd3e9f40d0943510a
hyebin <hyebin@DESKTOP-KE3UFEG>
2020-01-08 16:00:11 +0900

    혜빈

--- myprj2/src/user/view/content/MyDataView.java
+++ myprj2/src/user/view/content/MyDataView.java
@@ -1,5 +1,7 @@
 package user.view.content;
 
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,31 +15,34 @@ public class MyDataView extends JFrame{
 	
 	private JLabel jlMyPage;
 	private JTextArea jtaName;
-	private JButton jbtOrderList, jbtMyData, jbtWithdrawal, jbtOk, jbtLogOut;
+	private JButton jbtOrderList, jbtMyData, jbtWithdrawal, jbtOk, jbtLogOut, jbtJJim;
 	//카드등록, 찜하기, 주문내역, 내정보변경 
 	
-	public MyDataView() {
+	private static String id;
+	
+	public MyDataView(String id) {
 		super("마이페이지");
-		
+		this.id=id;
+
 		jlMyPage= new JLabel("마이페이지");
-		jtaName= new JTextArea("+회원님 환영합니다. \n");
+		jtaName= new JTextArea("'"+id+"'회원님 환영합니다. \n");
 		jbtOrderList= new JButton("주문내역");
 		jbtMyData= new JButton("내 정보 변경");
-		jbtWithdrawal= new JButton("탈퇴");
+		jbtWithdrawal= new JButton("카드등록");
+		jbtJJim= new JButton("찜한 상품");
 		jbtOk= new JButton("확인");
 		jbtLogOut= new JButton("로그아웃");
 		
-		
-		//이미지 
+		//이미지 // 광고 
 		ImageIcon iImage= new ImageIcon("C:\\dev\\workspace\\myproject2\\src\\clientView\\예시.PNG");
 		JLabel jtaImage= new JLabel(iImage);
 		//수동
 		setLayout(null);
 		jlMyPage.setBounds(10, 30, 100, 40);
-		jtaName.setBounds(40, 100, 250, 80);
-		jbtOrderList.setBounds(40, 180, 125, 80);
-		jbtMyData.setBounds(165, 180, 125, 80);
-		jbtWithdrawal.setBounds(40, 260, 250, 60);
+		jtaName.setBounds(40, 70, 250, 50);
+		jbtOrderList.setBounds(40, 140, 250, 60);//주문내역
+		jbtMyData.setBounds(40, 210, 250, 60);//내정보변경
+		jbtWithdrawal.setBounds(40, 260, 250, 60); //카드등록
 		jbtOk.setBounds(50,380,100,40);
 		jbtLogOut.setBounds(180,380,100,40);
 		jtaImage.setBounds(315, 100, 160, 300);
@@ -67,7 +72,7 @@ public class MyDataView extends JFrame{
 
 	
 	public static void main(String[] args) {
-		new MyDataView();
+		new MyDataView("hyebin");
 	}//main
 
 
