4bf3408461b4ff80694540e574230bdd0188bd32
SAMSUNG <SAMSUNG@SAMSUNG-PC>
2020-01-16 19:18:34 +0900

    수정

--- myprj2/src/admin/run/AdminRun.java
+++ myprj2/src/admin/run/AdminRun.java
@@ -6,7 +6,7 @@ import admin.thread2.FileServer;
 public class AdminRun {
 	public static void main(String[] args) {
 		FileServer fs = new FileServer();
-		fs.start();//
+		fs.start();// 파일 업로드
 		new LoginView();
 	}// main
 }// class
